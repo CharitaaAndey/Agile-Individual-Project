4220e31062a5f7757ff7a759c420bcfab7d8cfe6
CharitaaAndey <sa2739@njit.edu>
2023-08-11 15:01:08 -0400

    changing according to hello professor

--- tests/project.spec.js
+++ tests/project.spec.js
@@ -1,25 +1,31 @@
 const { test, expect } = require('@playwright/test');
 
-// Change this URL to match your local Next.js server's URL
 const nextjstest = 'http://localhost:3000/';
 
-test('Homepage content', async ({ page }) => {
+test('Page title should be "Hello Professor"', async ({ page }) => {
   // Navigate to the homepage
   await page.goto(nextjstest);
 
-  // Verify the presence of heading and paragraph elements
-  const heading = await page.$('h1');
-  const paragraph = await page.$('p');
+  // Verify the title text in the browser tab
+  const pageTitle = await page.title();
+  expect(pageTitle).toBe('Hello Professor');
+});
 
-  // Assert that the heading and paragraph elements are present
-  expect(heading).toBeTruthy();
-  expect(paragraph).toBeTruthy();
+test('Page title matches Figma mockup', async ({ page }) => {
+  // Navigate to the homepage
+  await page.goto(nextjstest);
 
-  // Get the text content of the heading and paragraph elements
-  const headingText = await heading.textContent();
-  const paragraphText = await paragraph.textContent();
+  // Verify the page title font, size, color, etc. (use your Figma specs)
+  // You might need to use selectors that correspond to the title element in the Figma design
 
-  // Verify the text content of the elements
-  expect(headingText).toBe('Welcome to My Web System!');
-  expect(paragraphText).toBe('This is the homepage of our web system project.');
+  // For example (this is a hypothetical example)
+  const titleElement = await page.$('.title-selector');
+  const titleFont = await titleElement.evaluate(el => getComputedStyle(el).fontFamily);
+  const titleSize = await titleElement.evaluate(el => getComputedStyle(el).fontSize);
+  const titleColor = await titleElement.evaluate(el => getComputedStyle(el).color);
+
+  // You can add more assertions for other design properties as needed
+  expect(titleFont).toBe('ExpectedFontFamily');
+  expect(titleSize).toBe('ExpectedFontSize');
+  expect(titleColor).toBe('ExpectedColor');
 });
