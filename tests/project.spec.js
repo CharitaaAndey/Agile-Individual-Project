24cab6d28e0201b0c98b1101106b975347c93f5e
CharitaaAndey <sa2739@njit.edu>
2023-08-11 14:26:45 -0400

    installing playwright with typescript

--- tests/project.spec.js
+++ tests/project.spec.js
@@ -1,7 +1,25 @@
-import { test, expect } from '@playwright/test';
+const { test, expect } = require('@playwright/test');
 
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.getByRole('heading', { name: 'Welcome to My Web System!' }).click();
-  await page.getByText('This is the homepage of our web system project.').click();
+// Change this URL to match your local Next.js server's URL
+const nextjstest = 'http://localhost:3000/';
+
+test('Homepage content', async ({ page }) => {
+  // Navigate to the homepage
+  await page.goto(nextjstest);
+
+  // Verify the presence of heading and paragraph elements
+  const heading = await page.$('h1');
+  const paragraph = await page.$('p');
+
+  // Assert that the heading and paragraph elements are present
+  expect(heading).toBeTruthy();
+  expect(paragraph).toBeTruthy();
+
+  // Get the text content of the heading and paragraph elements
+  const headingText = await heading.textContent();
+  const paragraphText = await paragraph.textContent();
+
+  // Verify the text content of the elements
+  expect(headingText).toBe('Welcome to My Web System!');
+  expect(paragraphText).toBe('This is the homepage of our web system project.');
 });
